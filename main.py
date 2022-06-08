91820fd47e3d7365b462bce9c7090fb91c50da5b
MaximShaybakov <momon_boton@mail.ru>
2022-06-08 17:02:11 +0500

    Second commit

--- main.py
+++ main.py
@@ -7,15 +7,39 @@ def open_csv():
     with open('phonebook_raw.csv') as f:
         rows = csv.reader(f, delimiter=',')
         cont_ls = list(rows)
+        # print(cont_ls)
     return cont_ls
 
 def data():
-    pattern = '\s'
-    for num,data in enumerate(open_csv()):
-        f_name = data[0] #.split()
-        pprint(f_name)
-        first_name = re.findall('')
+    initials_ = []
+    for num,name in enumerate(open_csv()):
+        text = ' '.join(name)
+        initials_pattern = r'(^[А-Я]{1}[а-я]+)(\s?)([А-Я]{1}[а-я]+)(\s?)([А-Я]{1}[а-я]+)'
+        init = re.search(initials_pattern, text)
+        if init is not None:
+            initials_.append(init.group(1, 3, 5))
+    return initials_
+
+def phone():
+    phone_list = []
+    for num,tel in enumerate(open_csv()):
+        text = ' '.join(tel)
+        phone_pattern = r'(\.?)([7|8]{1})(\s?)(\(?)(\d{3})(\s?)(\)?)(\W?)(\d{3})(\W?)(\d{2})(\W?)(\d*)'
+        number = re.search(phone_pattern, text)
+        if number is not None:
+            tel_number = f'+7({number.group(5)}){number.group(9)}-{number.group(11)}-{number.group(13)}'
+            phone_list.append(tel_number)
+    done = [data() for name,num in data(),phone_list]
+    return phone_list
 
 
+def ls():
+    done = []
 
-data()
+
+if __name__ == '__main__':
+    pprint(data())
+    pprint(phone())
+
+
+# (\W)(\(?)(доб.)(\W)(\d*)
