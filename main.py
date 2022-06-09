c480af09293690cc98f85e7ac4c19c319bfb9ca9
MaximShaybakov <momon_boton@mail.ru>
2022-06-09 10:11:33 +0500

    добавлена функция коррекции

--- main.py
+++ main.py
@@ -2,42 +2,62 @@ import csv
 import re
 from pprint import pprint
 
+def open_file():
+    with open('phonebook_raw.csv', 'r', encoding='utf-8') as csvfile:
+        rows_list_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        rows_list = []
+        for index, row in enumerate(rows_list_reader):
+            rows_list.append(row)
+    with open('phonebook_raw.csv', 'r', encoding='utf-8') as csvfile:
+        rows_dic_reader = csv.DictReader(csvfile, delimiter=',', quotechar='"') # quoting=csv.QUOTE_MINIMAL
+        rows_dict = []
+        for index1, row1 in enumerate(rows_dic_reader):
+            rows_dict.append(row1)
+    return (rows_list, rows_dict)
 
-def open_csv():
-    with open('phonebook_raw.csv') as f:
-        rows = csv.reader(f, delimiter=',')
-        cont_ls = list(rows)
-        # print(cont_ls)
-    return cont_ls
+# rows_list, rows_dict = open_file()
 
-def data():
-    initials_ = []
-    for num,name in enumerate(open_csv()):
-        text = ' '.join(name)
-        initials_pattern = r'(^[А-Я]{1}[а-я]+)(\s?)([А-Я]{1}[а-я]+)(\s?)([А-Я]{1}[а-я]+)'
-        init = re.search(initials_pattern, text)
-        if init is not None:
-            initials_.append(init.group(1, 3, 5))
-    return initials_
+
+def names():
+    person_list = []
+    for person_data in open_file()[0]:
+        data = ' '.join(person_data)
+        # pprint(data)
+        initials_pattern = r'([А-У]{1}\w*)\W([А-У]{1}\w*)\W([А-У]{1}\w*)'
+        initials = re.match(initials_pattern, data)
+        if initials is not None:
+            last_fir_sur = initials.group()
+            # print(last_fir_sur)
+            person_list.append(last_fir_sur.split())
+    return person_list
+
 
 def phone():
     phone_list = []
-    for num,tel in enumerate(open_csv()):
-        text = ' '.join(tel)
-        phone_pattern = r'(\d{3})(\s?)(\)?)(\W?)(\d{3})(\W?)(\d{2})(\W?)(\d*)((\s?)(\(?)(доб?\.?)(\s?)(\d{4}?))*'
-        phone_num = re.search(phone_pattern, text)
-        if phone_num is not None:
-            phone_list.append(f'+7({phone_num.group(1)}){phone_num.group(5)}-{phone_num.group(7)}-{phone_num.group(9)} {phone_num.group(13)}{phone_num.group(15)}')
+    for person_data in open_file()[0]:
+        data = ' '.join(person_data)
+        phone_pattern = r'(\d{1})\s?\(?(\d{3})\s?\)?\W?(\d{3})\W?(\d{2})\W?(\d+)(\s?\(?(доб\.)\s?(\d*))*'
+        phones = re.search(phone_pattern, data)
+        if phones is not None:
+            tel = f'+7({phones.group(2)}){phones.group(3)}-{phones.group(4)}-{phones.group(5)} {phones.group(7)}{phones.group(8)}'
+            phone_list.append(tel.replace('NoneNone', '').strip())
     return phone_list
 
 
-def ls():
-    done = []
+def correction_data():
+    count = 0
+    for index,_ in enumerate(open_file()[1]):
+        _['lastname'] = names()[count][0]
+        _['firstname'] = names()[count][1]
+        _['surname'] = names()[count][2]
+        _['phone'] = phone()
+        count += 1
+        print(_)
 
+    return
 
 if __name__ == '__main__':
-    # pprint(data())
-    pprint(phone())
-
-
-# (\W)(\(?)(доб.)(\W)(\d*)
+    # print(open_file())
+    # print(names())
+    print(phone())
+    # correction_data()
