3395ac0d55c79627852d963e03480184b055a75d
MaximShaybakov <momon_boton@mail.ru>
2022-06-12 19:02:20 +0500

    new commit

--- test.py
+++ test.py
@@ -11,15 +11,22 @@ with open('data_file/phonebook_raw.csv', 'r', encoding='utf-8') as csvfile:
     for _, row1 in enumerate(rows_dic_reader):
         rows_dict.append(row1)
 
-# list comprehenshion
-name_ls = set([ls['lastname'].split()[0] for ls in rows_dict])
 
+new_ls_rowdict = []
+for index, new_key in enumerate(rows_dict):
+    new_key['lastname'] = new_key['lastname'].split()[0]
+    new_ls_rowdict.append(new_key)
+pprint(new_ls_rowdict)
+
+
+# list comprehension
+# name_ls = set([ls['lastname'].split()[0] for ls in rows_dict])
 
 new = {}
-ls = []
+new_ls = []
 for person_data in rows_dict:
-    new.setdefault(person_data['lastname'].split()[0], \
-        [person_data['organization'], person_data['position'], person_data['phone'], person_data['email']])
-
+    new.setdefault(person_data['lastname'].split()[0], [person_data['organization'], person_data['position'], person_data['phone'], person_data['email']])
+    new_ls.append(person_data)
 pprint(new)
+
 
